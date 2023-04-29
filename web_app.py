846853ed53843352d922dc7f76e1798a5959da0d
ShriramDayama <91780071+ShriramDayama@users.noreply.github.com>
2023-04-29 12:56:57 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,13 +1,16 @@
 from streamlit_webrtc import webrtc_streamer
 import av
+import cv2
 
-def video_frame_callback(frame):
-  img = frame.to_ndarray(forrmat="bgr24")
-  retrun av.VideoFrame.from_ndarray(img, format="bgr24")
+
+class VideoProcessor:
+  def recv(self, frame):
+    frm = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 webrtc_streamer(key="sample" , 
                 rtc_configuration= {
                     "iceServers" : [{"urls":["stun:stun.l.google.com:19302"]}]
                 },
-                video_frame_callback=video_frame_callback
+                video_processor_factory=VideoProcessor
                 )
