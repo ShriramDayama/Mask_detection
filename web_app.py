39ace117873997f78875b07645bd6bbd48198f67
ShriramDayama <91780071+ShriramDayama@users.noreply.github.com>
2023-04-29 12:44:48 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,7 +1,13 @@
 from streamlit_webrtc import webrtc_streamer
+import av
+
+def video_frame_callback(frame):
+  img = frame.to_ndarray(forrmat="bgr24")
+  retrun av.VideoFrame.from_ndarray(img, format="bgr24")
 
 webrtc_streamer(key="sample" , 
                 rtc_configuration= {
                     "iceServers" : [{"urls":["stun:stun.l.google.com:19302"]}]
-                }
+                },
+                video_frame_callback=video_frame_callback
                 )
